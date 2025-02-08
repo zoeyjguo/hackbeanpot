a1d867285b781fed659b70e1db1e4b6dc6e21e60
Eric Sun <ericsun828@gmail.com>
2025-02-08 02:17:26 -0500

    add modify private playlist scope

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,21 @@ function App() {
   const REDIRECT_URI = "http://localhost:5173"
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
+  const SCOPES = [
+    "playlist-modify-private",
+  ]
 
   return (
     <div className="App">
       <header className="App-header">
-        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+        <a
+          className="App-link"
+          href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPES.join("%20")}&response_type=${RESPONSE_TYPE}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Login with Spotify
+        </a>        
       </header>
     </div>
   )

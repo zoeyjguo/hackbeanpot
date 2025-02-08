0dd4bf26c38facf39cca549efd9cc33cb1641b2d
Zoey Guo <zoeyjguo@gmail.com>
2025-02-08 13:05:40 -0500

    added log out implementation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from 'react';
 import './App.css'
+import logo from './assets/Primary_Logo_green_RGB.svg'
 
 function App() {
   const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
@@ -9,20 +11,45 @@ function App() {
     "playlist-modify-private",
   ]
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPES.join("%20")}&response_type=${RESPONSE_TYPE}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Login with Spotify
-        </a>        
-      </header>
-    </div>
-  )
-}
+  const [token, setToken] = useState("")
 
-export default App
+      useEffect(() => {
+          const hash = window.location.hash
+          let token = window.localStorage.getItem("token")
+
+          if (!token && hash) {
+              token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token"))!.split("=")[1]
+
+              window.location.hash = ""
+              window.localStorage.setItem("token", token)
+          }
+
+          setToken(token!)
+
+      }, [])
+
+      const logout = () => {
+        setToken("");
+        window.localStorage.removeItem("token"); 
+      };
+
+      const login = () => {
+        window.location.href = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPES.join("%20")}&response_type=${RESPONSE_TYPE}&show_dialog=true`
+      }
+
+      return (
+          <div className="App">
+              <header className="App-header">
+                  <img src={logo} width="200" height="200" alt="Spotify Logo" />
+                  <h1>Spotify React</h1>
+                  {!token ?
+                      <a className = "App-link" rel="noopener noreferrer">
+                        <button onClick={login}>Login with Spotify</button>
+                      </a>
+                      : <><h3> Hi {CLIENT_ID}! </h3><button onClick={logout}>Logout</button></>}
+              </header>
+          </div>
+      );
+  }
+
+export default App;

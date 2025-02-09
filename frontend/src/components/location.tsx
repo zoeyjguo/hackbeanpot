c086e6db69419cea1fe9031195f08823bc56f911
Eric Sun <ericsun828@gmail.com>
2025-02-08 22:37:46 -0500

    add location autocomplete

--- frontend/src/components/location.tsx
+++ frontend/src/components/location.tsx
@@ -1,13 +1,24 @@
 import Navbar from "./navbar";
-import TravelPage from "./TravelPage";
+import Autocomplete from 'react-google-autocomplete';
+
 
 const Location = () => {
     return (
         <div>
             <Navbar />
             <div className="min-h-screen bg-gray-100 py-10">
-          <TravelPage />
-        </div>
+                {/* <TravelPage /> */}
+                <div className="min-h-screen bg-gray-100 py-10 flex justify-center">
+                <div style={{ width: "300px" }}>
+                    <Autocomplete
+                        apiKey={import.meta.env.VITE_GOOGLE_MAP_API_KEY}
+                        onPlaceSelected={(place) => console.log("Selected Place:", place)}
+                        options={{ types: ["geocode"] }}
+                        style={{ width: "100%", padding: "10px", fontSize: "16px" }}
+                    />
+                </div>
+            </div>
+            </div>
         </div>
     )
 }
